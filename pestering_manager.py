fec27160b42be84be30ac791a2287a371a81652a
logston <paul.logston@gmail.com>
2013-08-03 00:17:23 +0000

    Updated run_pestering_manager to exit with error if python interpreter and petering_manager.py are not found. Added PesteringAttempt database insertions. Currently testing.

--- pestering_manager.py
+++ pestering_manager.py
@@ -5,9 +5,13 @@ of all pesterings that are due to be sent.
 This script is called by a cronjob every minute.
 """
 
+from datetime import datetime
+from random import randint
+
+from models import PesteringAttempt
+
 from pesterutils import bingapi
 from pesterutils import sendpester
-from random import randint
 
 DIR = '/home/paul/djprojs/mysite/pester/priv/'
 
@@ -21,5 +25,12 @@ bobj = bingapi.BingAPI(bing_api_key)
 d = bobj.query('Fat Cats')['d']['results']
 url = d[randint(0, len(d)-1)]['MediaUrl']
 
+pestering_attempt = PesteringAttempt(pestering=1, 
+                                     pestering_manager_run=1,
+                                     image=url,
+                                     attempt_time=datetime.now())
+pestering_attempt.save()
 p = sendpester.SendPester()
 p.send_pester(user, url)
+pestering_attempt.success = True
+pestering_attempt.save()
--- run_pestering_manager.sh
+++ run_pestering_manager.sh
@@ -3,8 +3,14 @@
 python=/home/paul/.virtualenvs/dj/bin/python
 manager=/home/paul/djprojs/mysite/pester/pestering_manager.py
 
-if [ -f $python]; then
-	
+if ! [ -f $python ]; then
+    echo "No python executable at $python"
+    exit 1	
+fi
+
+if ! [ -f $manager ]; then
+    echo "No pester manager found at $manager"
+    exit 1
 fi
 
 exec $python $manager
